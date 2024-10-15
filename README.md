68c4d5201dd2d2ff96ee5bb73eeb2f315b95e571
rafacoelho4 <rafaelmalto@hotmail.com>
2024-10-15 15:58:11 -0300

    Initial project

--- README.md
+++
@@ -1,2 +0,0 @@
-# qrcode_generator
-A QR Code generator developed using Java, Maven, Typescript and ReactJS. 
---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <div>
+            QR Code Generator
+        </div>
+
+        <nav>
+            Documentation
+        </nav>
+    </div>
+  );
+};
+
+export default Header; 
